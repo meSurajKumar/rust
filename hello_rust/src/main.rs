10d03d6a9dbdc61558515ecd2e2fb26e7a53dce9
meSurajKumar <mesuraj751@gmail.com>
2026-05-04 22:23:38 +0530

    added functions , params returns

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,102 +1,25 @@
 fn main() {
-    // println!("Hello, world!");
-    // let mut x = 5;
-    // x = x + 1;
-    
-    //  shadowing.
-    // let x = 5;
-    // let x = "userName";
-    // println!("value of x is : {}" , x)
+//  greet("Raj");
+// let result = square(2);
+// println!("square is : {}", result)
+let result = move_example();
+println!("result : {}", result)
+}
 
-    // this is just a traditional controll statement.
-    // let number = 7;
-    // if number < 5{
-    //     println!("number is less the 5")
-    // }else if number > 5 {
-    //     println!("number is grater then 5")
-    // }else {
-    //     println!("you are lucky")
-    // } 
+// // fn greet(name : &str){
+// //     println!("Hello ,{}!", name)
+// // }  
+
+// fn square(x : i32)-> i32{
+//     // println!("x  : {}", x)
+//     x * x
+// }
 
 
-    // this is just in the expression form
-    // let condition = false;
-    // // let value = if condition {println!("your condtion is true")} else {println!("your condtion is false")};
-    // let value = if condition {true} else {false};
-    // println!("value is {}", value)
-
-    // loop example 
-    // let mut count = 0;
-    // loop {
-    //     count += 1;
-    //     println!("count is {}", count);
-    //     if count == 10 {
-    //         break;
-    //     }
-    // };
-
-    // loop returning a value.    
-    // let mut tick = 0;
-    // let result = loop {
-    //     println!("{}",tick);
-    //     tick += 1;
-    //     if tick == 42{
-    //         break tick;
-    //     };
-    // };
-    // println!("the value is {}", result)
-
-
-    // let mut count = 0;
-    // loop {
-    //     count += 1;
-    //     println!("count : {} ", count);
-    //     if count == 5{
-    //         break;
-    //     }
-    // } 
-
-
-    // let mut tik = 0;
-    // let result = loop{
-    //     tik +=1;
-    //     println!("tik  : {}", tik);
-    //     if tik == 7{
-    //         break tik;
-    //     };
-    // };
-    // println!("this is result value : {}", result);
-
-    // while loop
-    // let mut n = 0;
-    // while n <5{
-    //     println!("value of n : {}", n);
-    //     n +=1;
-    // }
-
-    // for loop.
-    // for i in 0..5{
-    //     println!("i : {}", i)
-    // }
-    
-
-    // match example
-    let status = 500;
-    match status {
-        200 => println!("OK"),
-        404 => println!("Not found!"),
-            _=>println!("Unkonwn status"),
-    }
-
-
-
-
-
-
-
-
-
-
-
-
+// Borowing concept : 
+fn move_example() -> String{
+    let s1 = String::from("Hello");
+    let s2  = s1;
+    s2
+    // println!("{}",s1)
 }
