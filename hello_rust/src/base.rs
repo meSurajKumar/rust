28fc1f1c05612d4c756209e4135d98605f3d7d31
meSurajKumar <mesuraj751@gmail.com>
2026-05-05 23:50:31 +0530

    added ownership part-1

--- hello_rust/src/base.rs
+++ hello_rust/src/base.rs
@@ -1,3 +1,29 @@
+fn main() {
+//  greet("Raj");
+// let result = square(2);
+// println!("square is : {}", result)
+let result = move_example();
+println!("result : {}", result)
+}
+
+// // fn greet(name : &str){
+// //     println!("Hello ,{}!", name)
+// // }  
+
+// fn square(x : i32)-> i32{
+//     // println!("x  : {}", x)
+//     x * x
+// }
+
+
+// Borowing concept : 
+// fn move_example() -> String{
+//     let s1 = String::from("Hello");
+//     let s2  = s1;
+//     s2
+    // println!("{}",s1)
+// }
+
 // 1. Ownership (Malikana Haq)
 // Rust mein har value ka ek, aur sirf ek, Owner hota hai.
 
@@ -6,10 +32,4 @@
 // The Drop: Jab owner ka scope (jaise function ya if-block) khatam hota hai, toh Rust automatically ek drop function call karta hai, aur Heap memory ko turant free (deallocate) kar deta hai.
 
 // The Move: Agar tum ek variable ka data dusre variable ko dete ho (let b = a;), toh C++ ya JavaScript ki tarah yahan dono variable valid nahi rehte. Ownership pehle se dusre mein Move ho jati hai. Pehla variable wahi mar jata hai. Use dobara access karoge toh compiler error dega.
-
-fn main(){
-    // println!("hellow!");
-
-}
-
 
