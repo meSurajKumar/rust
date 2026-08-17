a27bf1ec09d30ecd2d9f94e640b28ced2ff8d5b1
meSurajKumar <mesuraj751@gmail.com>
2026-08-17 23:42:57 +0530

    feat: initialize rust project with sample code for pattern matching and lifetime exercises

---
+++ hello_rust/src/chapter_1/lifetime.rs
@@ -0,0 +1,20 @@
+// fn main(){
+//     let x = 10;
+//     let r = &x;
+//     println!("{}",r);
+// }
+
+// 1. referece is alive
+// 2. original value is valid/alive
+// 3. reference will never point to data which is already destroyed/dead
+
+// fn main(){
+//     let r;
+//     {
+//         let x = 10;
+//         r = &x;
+//     }
+//     println!{"{}",r}
+// }
+
+
